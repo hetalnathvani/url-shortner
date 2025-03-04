7a4733f7d1c42e342ff108343cb38763fae3a0f6
Hetal Nathvani <nathvanihetal@gmail.com>
2025-03-04 17:39:44 +0530

    Route based changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { createBrowserRouter, Link, RouterProvider } from "react-router-dom";
 import "./App.css";
+import Landing from "./pages/landing";
+import AppLayout from "./layouts/app-layout";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import RedirectLink from "./pages/redirect-link";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Landing />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/auth",
+        element: <Auth />,
+      },
+      {
+        path: "/link/:id",
+        element: <Link />,
+      },
+      {
+        path: "/:id",
+        element: <RedirectLink />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div className="text-2x1"></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
