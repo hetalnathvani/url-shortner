331f8c6c274a5701d75fafd301734784d1ea66df
Hetal Nathvani <nathvanihetal@gmail.com>
2025-03-05 13:32:18 +0530

    Header component design and other changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,3 @@
-import { Link, useNavigate } from "react-router-dom";
-import { Button } from "./ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,35 +5,63 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@radix-ui/react-dropdown-menu";
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
 
 const Header = () => {
   const navigate = useNavigate();
   const user = false;
-  return (
-    <nav className="py-4 flex justify-between items-center">
-      <Link to="/">
-        <img src="/logo.png" className="h-16" alt="Trimrr Logo" />
-      </Link>
 
-      <div>
-        {!user ? (
-          <Button onClick={() => navigate("/auth")}>Login</Button>
-        ) : (
-          <DropdownMenu>
-            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
-      </div>
-    </nav>
+  return (
+    <>
+      <nav className="py-4 flex justify-between items-center">
+        <Link to="/">
+          <img src="/logo.png" className="h-16" alt="Trimrr Logo" />
+        </Link>
+        <div className="flex gap-4">
+          {!user ? (
+            <Button onClick={() => navigate("/auth")}>Login</Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+                <Avatar>
+                  <AvatarImage src={user?.user_metadata?.profile_pic} />
+                  <AvatarFallback>HN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>
+                  {/* {user?.user_metadata?.name} */}
+                  Hetal Nathvani
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to="/dashboard" className="flex">
+                    <LinkIcon className="mr-2 h-4 w-4" />
+                    My Links
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem
+                  onClick={() => {
+                    // fnLogout().then(() => {
+                    //   fetchUser();
+                    //   navigate("/auth");
+                    // });
+                  }}
+                  className="text-red-400"
+                >
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Logout</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </div>
+      </nav>
+    </>
   );
 };
 
