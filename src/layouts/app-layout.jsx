e0cd3d2265d3b33545de90de5139462805d8bbcf
Hetal Nathvani <nathvanihetal@gmail.com>
2025-03-05 13:32:58 +0530

    App layout based changes

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -4,8 +4,8 @@ import { Outlet } from "react-router-dom";
 const AppLayout = () => {
   return (
     <div>
-      <Header />
       <main className="min-h-screen container">
+        <Header />
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
