d544248757e5e1dd608dbf0dd26e5444962735c2
Hetal Nathvani <nathvanihetal@gmail.com>
2025-03-06 14:30:19 +0530

    Error handling in login page | DONE

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,3 +1,4 @@
+import { Input } from "./ui/input";
 import {
   Card,
   CardContent,
@@ -5,24 +6,38 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "./ui/input";
-import { BeatLoader } from "react-spinners";
+} from "./ui/card";
 import { Button } from "./ui/button";
-import Error from "./error";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import * as Yup from "yup";
-import useFetch from "@/hooks/use-fetch";
+import Error from "./error";
 import { login } from "@/db/apiAuth";
-import { useNavigate } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
+import useFetch from "@/hooks/use-fetch";
+import { UrlState } from "@/context";
 
 const Login = () => {
+  let [searchParams] = useSearchParams();
+  const longLink = searchParams.get("createNew");
+
   const [errors, setErrors] = useState({});
-  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+
+  const navigate = useNavigate();
+  const { loading, error, fn: fnLogin, data } = useFetch(login, formData);
+  const { fetchUser } = UrlState();
+
+  useEffect(() => {
+    if (error === null && data) {
+      fetchUser();
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error, data]);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -32,21 +47,12 @@ const Login = () => {
     }));
   };
 
-  useEffect(() => {
-    console.log(data);
-    if (error == null && data) {
-      navigate(`/dahsboard?${longLink ? `createNew=${longLink}` : ""}`);
-    }
-  }, [data, error]);
-
-  const { data, error, loading, fn: fnLogin } = useFetch(login, formData);
-
   const handleLogin = async () => {
     setErrors([]);
     try {
       const schema = Yup.object().shape({
         email: Yup.string()
-          .email("Invalid Email")
+          .email("Invalid email")
           .required("Email is required"),
         password: Yup.string()
           .min(6, "Password must be at least 6 characters")
@@ -67,42 +73,40 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>
-            to your account of you already have one
-          </CardDescription>
-          {error && <Error message={error.message} />}
-        </CardHeader>
-        <CardContent className="space-y-2">
-          <div className="space-y-1">
-            <Input
-              name="email"
-              type="email"
-              placeholder="Enter Email"
-              onChange={handleInputChange}
-            />
-            {error && <Error message={errors.email} />}
-          </div>
-          <div className="space-y-1">
-            <Input
-              name="password"
-              type="password"
-              placeholder="Enter Password"
-              onChange={handleInputChange}
-            />
-            {error && <Error message={errors.password} />}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button onClick={handleLogin}>
-            {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          to your account if you already have one
+        </CardDescription>
+        {error && <Error message={error.message} />}
+      </CardHeader>
+      <CardContent className="space-y-2">
+        <div className="space-y-1">
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter Email"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.email && <Error message={errors.email} />}
+        <div className="space-y-1">
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter Password"
+            onChange={handleInputChange}
+          />
+        </div>
+        {errors.password && <Error message={errors.password} />}
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleLogin}>
+          {loading ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
